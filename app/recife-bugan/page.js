b00f6f07a6ac3dcbfc67e6b18ca8c15c4527cb41
LoungeMilhas <eduardo@loungemilhas.com.br>
2026-05-29 15:55:41 -0300

    Update page.js

--- app/recife-bugan/page.js
+++ app/recife-bugan/page.js
@@ -1,1 +1,21 @@
+export default function RecifeBugan() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🏨 Recife - Bugan Boa Viagem</h1>
 
+      <p>Proposta Premium Lounge Milhas</p>
+
+      <p>
+        Período: 19/06/2026 a 22/06/2026
+      </p>
+
+      <p>
+        03 adultos + 01 bebê
+      </p>
+
+      <p>
+        02 quartos Standard Queen
+      </p>
+    </main>
+  );
+}
